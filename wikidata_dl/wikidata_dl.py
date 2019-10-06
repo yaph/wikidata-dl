35b4556f6470a973412b96b7aad411d77e53a976
Ramiro Gómez <code@ramiro.org>
2019-10-06 22:25:33 +0200

    Make download less complex by adding functions. Add function annotations.

--- wikidata_dl/wikidata_dl.py
+++ wikidata_dl/wikidata_dl.py
@@ -8,13 +8,15 @@ import requests
 import wptools
 
 from datetime import datetime, timezone
+from typing import List
+
 from dateutil.parser import parse as parsedate
 
 
 logging.basicConfig(filename='wikidata.log', level=logging.WARNING)
 
 
-def get_wikibase_ids(query):
+def get_wikibase_ids(query: str) -> List[str]:
     """Return a set of Wikibase IDs for given query from Wikidata."""
 
     params = {
@@ -29,19 +31,37 @@ def get_wikibase_ids(query):
         raise Exception('Data could not be fetched.')
 
 
-def download(wikibase_ids, cache_dir, cache_lifetime):
+def is_cached(file_name: str, cache_lifetime: int) -> bool:
+    """Check whether cache file exists is still valid."""
+
+    if os.path.exists(file_name):
+        file_modified = os.path.getmtime(file_name)
+        if (time.time() - file_modified < cache_lifetime):
+            logging.info(f'Cached file {file_name} is still valid.')
+            return True
+    return False
+
+
+def is_wikidata_newer(file_name: str, page: dict) -> bool:
+    """Check whether last Wikidata update is newer than cache file."""
+
+    if os.path.exists(file_name):
+        file_modified = os.path.getmtime(file_name)
+        if datetime.fromtimestamp(file_modified, tz=timezone.utc) > parsedate(page.data['modified']['wikidata']):
+            logging.info(f'Last wikidata update older than {file_name}.')
+            return False
+    return True
+
+
+def download(wikibase_ids: List[str], cache_dir: str, cache_lifetime: int) -> None:
     """Fetch and cache data for all Wikibase IDs passed to this function."""
 
     os.makedirs(cache_dir, exist_ok=True)
 
     for count, wikibase in enumerate(wikibase_ids):
         file_name = os.path.join(cache_dir, wikibase + '.json')
-        file_modified = None
-        if os.path.exists(file_name):
-            file_modified = os.path.getmtime(file_name)
-            if (time.time() - file_modified < cache_lifetime):
-                logging.info(f'Cached file {file_name} is still valid.')
-                continue
+        if is_cached(file_name, cache_lifetime):
+            continue
 
         wikidata_url = 'https://www.wikidata.org/wiki/' + wikibase
 
@@ -50,8 +70,7 @@ def download(wikibase_ids, cache_dir, cache_lifetime):
         page.get_wikidata()
 
         # Continue if last Wikidata update is older than last modification of JSON file
-        if file_modified and datetime.fromtimestamp(file_modified, tz=timezone.utc) > parsedate(page.data['modified']['wikidata']):
-            logging.info(f'Last wikidata update older than {file_name}.')
+        if is_wikidata_newer(file_name, page):
             continue
 
         # Make a copy to keep original values in case of redirects
